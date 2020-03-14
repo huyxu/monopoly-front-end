3c172ce439646ea0f6263f717ea378f72c047e23
NamDo <namdo10121999@gmail.com>
2020-03-14 10:33:30 +0700

    Add game data files: slotArr.js and slotSpecial.js

---
+++ src/data/slotArr.js
@@ -0,0 +1,263 @@
+const slotArr = [
+  // SLOT BOTTOM ROW
+  {
+    id: 0,
+    name: "Go",
+    slotRowPos: "10/11",
+    slotColPos: "10/11",
+    slotColor: ""
+  },
+  {
+    id: 1,
+    name: "Lào Cai",
+    slotRowPos: "10/11",
+    slotColPos: "9/10",
+    slotColor: "#757575"
+  },
+  {
+    id: 2,
+    name: "Khi Van",
+    slotRowPos: "10/11",
+    slotColPos: "8/9",
+    slotColor: ""
+  },
+  {
+    id: 3,
+    name: "Hòa Bình",
+    slotRowPos: "10/11",
+    slotColPos: "7/8",
+    slotColor: "#757575"
+  },
+  {
+    id: 4,
+    name: "Income Tax",
+    slotRowPos: "10/11",
+    slotColPos: "6/7",
+    slotColor: ""
+  },
+  {
+    id: 5,
+    name: "Hạ Long",
+    slotRowPos: "10/11",
+    slotColPos: "5/6",
+    slotColor: "#66BB6A"
+  },
+  {
+    id: 6,
+    name: "Chance",
+    slotRowPos: "10/11",
+    slotColPos: "4/5",
+    slotColor: ""
+  },
+  {
+    id: 7,
+    name: "Hải Phòng",
+    slotRowPos: "10/11",
+    slotColPos: "3/4",
+    slotColor: "#66BB6A"
+  },
+  {
+    id: 8,
+    name: "Hà Nội",
+    slotRowPos: "10/11",
+    slotColPos: "2/3",
+    slotColor: "#66BB6A"
+  },
+  {
+    id: 9,
+    name: "Visit Jail",
+    slotRowPos: "10/11",
+    slotColPos: "1/2",
+    slotColor: ""
+  },
+
+  // SLOT LEFT COL
+  {
+    id: 10,
+    name: "Hải Dương",
+    slotRowPos: "9/10",
+    slotColPos: "1/2",
+    slotColor: "#8D6E63"
+  },
+  {
+    id: 11,
+    name: "Thái Bình",
+    slotRowPos: "8/9",
+    slotColPos: "1/2",
+    slotColor: "#8D6E63"
+  },
+  {
+    id: 12,
+    name: "Nam Định",
+    slotRowPos: "7/8",
+    slotColPos: "1/2",
+    slotColor: "#8D6E63"
+  },
+  {
+    id: 13,
+    name: "Bus Stop",
+    slotRowPos: "6/7",
+    slotColPos: "1/2",
+    slotColor: ""
+  },
+  {
+    id: 14,
+    name: "Thanh Hóa",
+    slotRowPos: "5/6",
+    slotColPos: "1/2",
+    slotColor: "#AB47BC"
+  },
+  {
+    id: 15,
+    name: "Chest",
+    slotRowPos: "4/5",
+    slotColPos: "1/2",
+    slotColor: ""
+  },
+  {
+    id: 16,
+    name: "Vinh",
+    slotRowPos: "3/4",
+    slotColPos: "1/2",
+    slotColor: "#AB47BC"
+  },
+  {
+    id: 17,
+    name: "Hưng Yên",
+    slotRowPos: "2/3",
+    slotColPos: "1/2",
+    slotColor: "#AB47BC"
+  },
+  {
+    id: 18,
+    name: "Airport",
+    slotRowPos: "1/2",
+    slotColPos: "1/2",
+    slotColor: ""
+  },
+
+  // SLOT TOP ROW
+  {
+    id: 19,
+    name: "Huế",
+    slotRowPos: "1/2",
+    slotColPos: "2/3",
+    slotColor: "#5D4037"
+  },
+  {
+    id: 20,
+    name: "Chance",
+    slotRowPos: "1/2",
+    slotColPos: "3/4",
+    slotColor: ""
+  },
+  {
+    id: 21,
+    name: "Đà Nẵng",
+    slotRowPos: "1/2",
+    slotColPos: "4/5",
+    slotColor: "#5D4037"
+  },
+  {
+    id: 22,
+    name: "Hội An",
+    slotRowPos: "1/2",
+    slotColPos: "5/6",
+    slotColor: "#5D4037"
+  },
+  {
+    id: 23,
+    name: "Bus Stop",
+    slotRowPos: "1/2",
+    slotColPos: "6/7",
+    slotColor: ""
+  },
+  {
+    id: 24,
+    name: "Kom Tum",
+    slotRowPos: "1/2",
+    slotColPos: "7/8",
+    slotColor: "#00ACC1"
+  },
+  {
+    id: 25,
+    name: "Pleiku",
+    slotRowPos: "1/2",
+    slotColPos: "8/9",
+    slotColor: "#00ACC1"
+  },
+  {
+    id: 26,
+    name: "Đà Lạt",
+    slotRowPos: "1/2",
+    slotColPos: "9/10",
+    slotColor: "#00ACC1"
+  },
+  {
+    id: 27,
+    name: "Jail",
+    slotRowPos: "1/2",
+    slotColPos: "10/11",
+    slotColor: ""
+  },
+
+  // SLOT COL RIGHT
+  {
+    id: 28,
+    name: "Nha Trang",
+    slotRowPos: "2/3",
+    slotColPos: "10/11",
+    slotColor: "#00796B"
+  },
+  {
+    id: 29,
+    name: "Vũng Tàu",
+    slotRowPos: "3/4",
+    slotColPos: "10/11",
+    slotColor: "#00796B"
+  },
+  {
+    id: 30,
+    name: "Chest",
+    slotRowPos: "4/5",
+    slotColPos: "10/11",
+    slotColor: ""
+  },
+  {
+    id: 31,
+    name: "Hồ Chí Minh",
+    slotRowPos: "5/6",
+    slotColPos: "10/11",
+    slotColor: "#00796B"
+  },
+  {
+    id: 32,
+    name: "Bus Stop",
+    slotRowPos: "6/7",
+    slotColPos: "10/11",
+    slotColor: ""
+  },
+  {
+    id: 33,
+    name: "Chance",
+    slotRowPos: "7/8",
+    slotColPos: "10/11",
+    slotColor: ""
+  },
+  {
+    id: 34,
+    name: "Cần Thơ",
+    slotRowPos: "8/9",
+    slotColPos: "10/11",
+    slotColor: "#F06292"
+  },
+  {
+    id: 35,
+    name: "Phú Quốc",
+    slotRowPos: "9/10",
+    slotColPos: "10/11",
+    slotColor: "#F06292"
+  }
+];
+
+module.exports = { slotArr };
---
+++ src/data/slotSpecial.js
@@ -0,0 +1,100 @@
+// Cơ hội
+const chanceArr = [
+  {
+    id: 0,
+    content: "Trở về ô Start và nhận 2000$"
+  },
+  {
+    id: 1,
+    content: "Tiến thẳng tới Hải Phòng. Nhận 2000$ nếu đi qua ô Start"
+  },
+  {
+    id: 2,
+    content: "Chuyển đến ô Hải Dương"
+  },
+  {
+    id: 3,
+    content: "Nhận tiền mừng cưới $500"
+  },
+  {
+    id: 4,
+    content: "Tiến thêm 3 bước"
+  },
+  {
+    id: 5,
+    content: "Tiến đến ô đất trống gần nhất"
+  },
+  {
+    id: 6,
+    content: "Tiến đến ô đất gần nhất thuộc sở hữu của mình"
+  },
+  {
+    id: 7,
+    content: "Mua vé ra tù với giá $250"
+  },
+  {
+    id: 8,
+    content: "Chuyển đến ô Hưng Yên"
+  },
+  {
+    id: 9,
+    content: "Trúng xổ số $3000"
+  },
+  {
+    id: 10,
+    content: "Nhận thêm một lượt tung xúc sắc"
+  },
+  {
+    id: 11,
+    content: "Sinh con được mừng $1 từ tất cả bạn bè"
+  },
+  {
+    id: 12,
+    content: "Chuyển đến ô sân bay"
+  }
+];
+// Khí vận
+const chestArr = [
+  {
+    id: 0,
+    content: "Vô tù!"
+  },
+  {
+    id: 1,
+    content: "Bạn bị mất lượt tiếp theo{ Mua sắm đồ nội thất hết $1500"
+  },
+  {
+    id: 2,
+    content: "Chia tay người yêu khóc hết $1500"
+  },
+  {
+    id: 3,
+    content: "Đi du lịch Sa Pa cùng người yêu hết $500"
+  },
+  {
+    id: 4,
+    content: "Chuyển đến ô thăm tù "
+  },
+  {
+    id: 5,
+    content: "Tiến đến ô đất gần nhất của đối thủ"
+  },
+  {
+    id: 6,
+    content: "Lùi lại 6 ô"
+  },
+  {
+    id: 7,
+    content: "Mừng tân gia đối thủ $1000"
+  },
+  {
+    id: 8,
+    content: "Lùi về ô đất gần nhất của đối thủ"
+  },
+  {
+    id: 9,
+    content: "Chuyển đến ô thuế"
+  }
+];
+
+module.exports = { chestArr, chanceArr };
